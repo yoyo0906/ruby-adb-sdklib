5c7eb608ce3c207b417bac419da6a1685cb67756
encrypt <encrypt94@gmail.com>
2014-10-18 14:26:52 +0200

    add documentation

--- lib/adb_sdklib/raw_image.rb
+++ lib/adb_sdklib/raw_image.rb
@@ -1,9 +1,14 @@
 # coding: utf-8
 
 module AdbSdkLib
+
+  # Data representing an image taken from a device frame buffer.
+  #
+  # This is a wrapper of com.android.ddmlib.RawImage
   class RawImage
     include Common
 
+    # @param [Rjb::Rjb_JavaProxy] RawImage Rjb proxy of com.android.ddmlib.RawImage
     def initialize(image)
       unless image.instance_of?(Rjb::Rjb_JavaProxy) &&
           image._classname == 'com.android.ddmlib.RawImage'
@@ -21,10 +26,16 @@ module AdbSdkLib
       @image = image
     end
 
+    # Returns ARGB value of a pixel
+    # @param [Fixnum] index of the pixel in data
+    # @return [Fixnum] ARGB value of the given pixel
     def get_argb(index)
       @image.getARGB(index)
     end
 
+    # Returns Hash with color values of a pixel
+    # @param [Fixnum] index of the pixel in data
+    # @return [Hash] color values of the given pixel
     def color(index)
       argb = @image.getARGB(index)
       {
@@ -35,22 +46,34 @@ module AdbSdkLib
       }
     end
 
+    # Returns Hash with color values of a pixel
+    # @param [Fixnum, Fixnum] pixel position x,y
+    # @return [Hash] color values of the given pixel
     def color_at(x,y)
       color(point_to_index(x,y))
     end
 
+    # Returns image's width
+    # @return [Fixnum] image's width
     def width()
       @image.width
     end
 
+    # Returns image's height
+    # @return [Integer] image's height
     def height()
       @image.height
     end
 
+    # Returns image's bpp
+    # @return [Integer] image's bpp
     def bpp()
       @image.bpp
     end
 
+    # Returns pixel index for a given position
+    # @param [Integer, Integer] pixel position x,y
+    # @return [Integer] pixel index
     def point_to_index(x,y)
       return (x*(bpp >> 3))+(y*((bpp >> 3)*(width)))
     end
